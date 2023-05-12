25f881cf526a0f66181ad714e9fe28682d2339c9
Dominik Zalewski <dominikzalewsi@gmail.com>
2023-05-12 16:13:05 +0200

    Hero section phone update & offer section setup

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,20 @@
   <main>
     <Header />
     <Hero />
+    <Offer />
   </main>
 </template>
 
 <script>
 import Header from "./components/header/HeaderDefault.vue";
 import Hero from "./components/hero/HeroIndex.vue";
+import Offer from "./components/offer/offerIndex.vue";
 export default {
   name: "App",
   components: {
     Header,
     Hero,
+    Offer,
   },
 };
 </script>
